6dbdc48d2d3dc8c1b8cff8843c2884bbbe46162f
Vaishnavi Deshpande <your-email@example.com>
2025-11-19 20:19:28 +0530

    Initial project upload

---
+++ check.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("cotton_crop_disease_cleaned.csv")
+print(df.columns)
---
+++ templates/check.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("C:\Users\user\OneDrive\Desktop\Cotton\cotton_crop_disease_cleaned.csv")
+print(df.columns)
